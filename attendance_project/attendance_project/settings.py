0db03288b2e4edaa174b90de39491e167c23e4f7
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2025-05-04 21:10:16 -0400

    update

--- attendance_project/attendance_project/settings.py
+++ attendance_project/attendance_project/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Add this line to configure STATIC_ROOT
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where static files will be collected
+# Directory where static files will be collected
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line to specify the path
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
