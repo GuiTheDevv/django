00098cdbb657599326530c613fc26413f69866fa
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2025-05-04 21:12:19 -0400

    run

--- attendance_project/attendance_project/settings.py
+++ attendance_project/attendance_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Directory where static files will be collected
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line to specify the path
